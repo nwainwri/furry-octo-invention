222d9db0e9bc2a227757354f9dbf3ce711b9464d
Nathan Wainwright <nathanwainwright@gmail.com>
2018-08-05 16:29:47 -0700

    setup snakes/ladders dictionary with key:values for spots

--- SnekAndLeds/Classes/Player.m
+++ SnekAndLeds/Classes/Player.m
@@ -14,11 +14,25 @@
 {
     self = [super init];
     if (self) {
-//        NSDictionary *sneksLeds = {
-//                //ladders
-//            
-//        };
-//        _currentRoll = 0;
+        // ledds; lowKey to highValue
+        // sneks; highKey to lowValue
+        NSDictionary *sneksLeddz = @{
+                                     //@"footLadder": @"topLadder",
+                                     @4:@14,
+                                     @9:@31,
+                                     @20:@38,
+                                     @28:@84,
+                                     @41:@89,
+                                     @51:@67,
+                                     @63:@81,
+                                     //@"snakeHead": @"snakeTail",
+                                     @17:@7,
+                                     @64:@60,
+                                     @89:@26,
+                                     @95:@75,
+                                     @99:@78
+                                     };
+        _currentRoll = 0;
         _currentSquare = 0;
     }
     return self;
@@ -27,13 +41,13 @@
 
 -(void)rollDice{
     //never seems to pop out a '6' roll
-    self.currentRoll = arc4random_uniform(6)+1;
+    self.currentRoll = arc4random_uniform(6)+1; // INTEGER MUST BE WRAPPED IN NSNUMBER
     self.currentSquare = self.currentSquare + self.currentRoll;
-//    return self.currentRoll;
+    
 }
 
 
 
-//. NSNumber *yourNumber = [NSNumber numberWithInt:your_int_variable];  https://forums.macrumors.com/threads/converting-an-int-into-an-nsnumber-then-saving-to-an-array.547952/
+// NSNumber *yourNumber = [NSNumber numberWithInt:your_int_variable];  https://forums.macrumors.com/threads/converting-an-int-into-an-nsnumber-then-saving-to-an-array.547952/
 
 @end
--- SnekAndLeds/main.m
+++ SnekAndLeds/main.m
@@ -34,7 +34,7 @@ int main(int argc, const char * argv[]) {
             if ([userAnswerString isEqualToString:@"r"]) {
                 gameOn = YES;
                 [playerOne rollDice];
-                NSLog(@"     Current:[ %ld ] Roll:[ %d ]", (long)playerOne.currentSquare, playerOne.currentRoll);
+                NSLog(@"|    Current:[ %ld ] Roll:[ %d ]", (long)playerOne.currentSquare, playerOne.currentRoll);
                 
             }
             if ([userAnswerString isEqualToString:@"q"]) {
